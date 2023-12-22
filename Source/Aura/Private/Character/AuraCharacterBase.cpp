270d2cbf323dfef2e624b1487b3a8f4bb032ea77
wuyukwi <b1044763336@gmail.com>
2023-12-22 09:53:31 +0900

    new

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,17 @@
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), "WeaponHandSocket");
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
---
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AuraCharacterBase.generated.h"
+
+UCLASS(abstract)
+class AURA_API AAuraCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AAuraCharacterBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, Category = "Combat")
+	TObjectPtr<USkeletalMeshComponent> Weapon;
+};
